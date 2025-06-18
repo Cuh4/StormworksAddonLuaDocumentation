a1524e8b3cb3695fbbcce6fa25abb2e9077fc9e6
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2025-06-18 11:06:41 +0100

    feat: add `dump.py` to dump all documented functions into `dump.txt`

---
+++ dump.py
@@ -0,0 +1,40 @@
+"""
+----------------------------------------------
+StormworksAddonLuaDocumentation: An up-to-date Lua LSP meta file for Stormworks Addon Lua.
+https://github.com/Cuh4/StormworksAddonLuaDocumentation
+----------------------------------------------
+
+Copyright (C) 2025 Cuh4
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# rushed and hardcoded, but works
+
+# // Main
+with open("docs/intellisense.lua") as f:
+    lines = f.readlines()
+    
+funcs: list[str] = []
+
+for line in lines:
+    if line.startswith("server."):
+        func_name = line.split(" =")[0]
+        funcs.append(func_name)
+    
+    if line.startswith("function") and not line.startswith("function on") and not line.startswith("function httpReply"):
+        func_name = line.split()[1].split("(")[0]
+        funcs.append(func_name)
+
+with open("dump.txt", "w") as f:
+    f.write("\n".join(funcs))
